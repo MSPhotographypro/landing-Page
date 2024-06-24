17f37755f0f4988435644e730ca8810c9b0b36e8
MSPhotographypro <msphotography758028@gmail.com>
2024-06-24 11:09:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,17 +82,8 @@ function showSlidesWedding(n) { // Rename showSlides to showSlidesWedding
 document.addEventListener("DOMContentLoaded", function() {
   const imagesData = [
     ["./img/01.jpg", "./img/02.jpg", "./img/03.jpg"],
-    ["./img/04.jpg", "./img/05.jpg", "./img/01.jpg"],
-    ["./img/02.jpg", "./img/03.jpg", "./img/04.jpg"],
-    ["./img/01.jpg", "./img/02.jpg", "./img/03.jpg"],
-    ["./img/04.jpg", "./img/05.jpg", "./img/01.jpg"],
-    ["./img/02.jpg", "./img/03.jpg", "./img/04.jpg"],
-    ["./img/01.jpg", "./img/02.jpg", "./img/03.jpg"],
-    ["./img/04.jpg", "./img/05.jpg", "./img/01.jpg"],
-    ["./img/02.jpg", "./img/03.jpg", "./img/04.jpg"],
-    ["./img/01.jpg", "./img/02.jpg", "./img/03.jpg"],
-    ["./img/04.jpg", "./img/05.jpg", "./img/01.jpg"],
-    ["./img/02.jpg", "./img/03.jpg", "./img/04.jpg"],
+    ["./img/04.jpg", "./img/05.jpg", "./img/06.jpg"],
+    ["./img/07.jpg"],
   ];
 
   const sliderContainer = document.querySelector(".slider");
